087f701157e58ce59b1f467d3456fd1c8ed3eef5
Luan Fonseca <luanfonsecap@icloud.com>
2021-06-02 20:23:17 -0300

    created jwt token response

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CipherController } from './module/cipher/cipher.controller';
-import { CipherService } from './module/cipher/cipher.service';
+import { CryptController } from './module/cipher/crypt.controller';
+import { CryptService } from './module/cipher/crypt.service';
 import { ConfigModule } from '@nestjs/config';
+import { JwtController } from './module/cipher/jwt.controller';
+import { JwtService } from './module/cipher/jwt.service';
 
 @Module({
   imports: [
@@ -9,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.development.env',
     }),
   ],
-  controllers: [CipherController],
-  providers: [CipherService],
+  controllers: [CryptController, JwtController],
+  providers: [CryptService, JwtService],
 })
 export class AppModule {}
